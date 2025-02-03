05e60ff3fa984c05816fce952fc0c8373d0e5021
Tarik El fouhama <elfouhamatariq@gmail.com>
2025-02-04 00:24:35 +0100

    frontent and backend in same port

--- backend/index.js
+++ backend/index.js
@@ -3,15 +3,26 @@ import dotenv from 'dotenv';
 import { connectDB } from './db/config.js';
 import authRoute from './Routes/auth.route.js';
 import cookieParser from 'cookie-parser'
+import path from "path"
 import cors from "cors"
 dotenv.config();
 const app = express()
+const __dirname  =path.resolve()
 app.use(cors({
     origin:"http://localhost:5173", credentials:true
 }))
 app.use(express.json())
 app.use(cookieParser())
 app.use('/api/auth',authRoute)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'frontend/dist')))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend','dist', 'index.html'))
+    })
+}
+
 
 
 const port = process.env.PORT || 5000
